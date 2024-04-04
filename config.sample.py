6d828cbd29168c7d37ad23b17e1812a4d626761d
Christoph Sonntag <info@christophsonntag.com>
2024-04-04 23:22:00 +0200

    Update config.py

--- config.sample.py
+++ config.sample.py
@@ -1,7 +1,8 @@
 HERMES_CONFIG = {
     "hf_access_token": "<Hugging Face access token here>",
     "hf_inference_endpoint": "https://api-inference.huggingface.co/pipeline/<model path>",
-    "vector_size": 384,
+    "embedder_identifier": "thenlper/gte-large",
+    "vector_size": 1024,
     "mongodb": {
         "host": "localhost",
         "port": 27017
